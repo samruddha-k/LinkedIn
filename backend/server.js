e0180b37244b1f8e077c156e4d01c43aa325112f
samruddha-k <samruddha-k@users.noreply.github.com>
2026-04-11 00:14:29 +0530

    Use process.env.PORT for backend server listen port

--- backend/server.js
+++ backend/server.js
@@ -260,8 +260,9 @@ async function start() {
     })
   );
 
-  app.listen(4000, () =>
-    console.log("🚀 Server running at http://localhost:4000/graphql")
+  const port = process.env.PORT || 4000;
+  app.listen(port, () =>
+    console.log(`🚀 Server running at http://localhost:${port}/graphql`)
   );
 }
 
